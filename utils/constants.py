96f9495c6b6dfcea0606049d969573a104a1c4fd
umang <umang.rastogi96@gmail.com>
2020-03-20 19:38:46 -0400

    Add comments for code documentation

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,12 @@
+# Define minimum distance threshold in map
 dist_thresh = 0.5
 scaling_factor = int(1 / dist_thresh)
+# Define threshold around goal
 goal_thresh = int(scaling_factor * 1.5)
+# Define map size
 width, height = 300, 200
+# Define all the possible no. of actions
 max_actions = 5
 half_actions = max_actions // 2
+# Define total angle of a complete circle
+total_angle = 360
