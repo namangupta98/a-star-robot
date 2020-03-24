ab018e9444708727a77f64e8312c8dbd49beb6c0
umang <umang.rastogi96@gmail.com>
2020-03-24 16:01:25 -0400

    Add projects constants for exploration of map

--- utils/constants.py
+++ utils/constants.py
@@ -5,8 +5,13 @@ scaling_factor = int(1 / dist_thresh)
 goal_thresh = int(scaling_factor * 1.5)
 # Define map size
 width, height = 300, 200
+map_size = (scaling_factor * height), (scaling_factor * width)
 # Define all the possible no. of actions
 max_actions = 5
 half_actions = max_actions // 2
 # Define total angle of a complete circle
 total_angle = 360
+# Define exploration constants
+no_parent = -1
+node_generated = 1
+start_parent = -99
